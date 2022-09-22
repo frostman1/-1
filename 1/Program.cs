b41d1f4ca4e0facf3661e79153739a69a9883520
frostman1 <113232289+frostman1@users.noreply.github.com>
2022-09-23 00:41:38 +0300

    1 задание

---
+++ 1/Program.cs
@@ -0,0 +1,20 @@
+﻿/*Задача 2: Напишите программу, которая на вход принимает два числа и выдаёт, 
+какое число большее, а какое меньшее.
+a = 5; b = 7 -> max = 7
+a = 2 b = 10 -> max = 10
+a = -9 b = -3 -> max = -3*/
+
+Console.WriteLine("Ввести число :");
+int number_1 = int.Parse(Console.ReadLine());
+int number_2 = int.Parse(Console.ReadLine());
+
+if (number_1>number_2) {
+    Console.WriteLine($"max - {number_1}");
+}
+else if(number_1<number_2) {
+    Console.WriteLine($"max-{number_2}");
+} else  {
+    Console.WriteLine ("долбаёб они равны");
+}
+
+
